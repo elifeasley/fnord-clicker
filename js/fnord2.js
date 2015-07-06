561b959efbff362c228871084b6a2e4851fb980d
Eliana Feasley <eliana@khanacademy.org>
2015-07-06 20:56:46 +0000

    gaining ability to even

--- js/fnord2.js
+++ js/fnord2.js
@@ -8,6 +8,7 @@ function fnord() {
 }
 
 $('html').click(function(e) {
+    var target = $(e.target), article;
     random = Math.random();
     count += random < Math.PI / (13 * 13)
         ? 1 + Math.random()
@@ -19,18 +20,22 @@ $('html').click(function(e) {
         count = Math.round(count) - 2;
     }
     
+     if (target.is(".chaos")) {
+         count = Math.random() > .5 ? count * 10 : 0;
+        $(".chaos")
+            .animate({marginTop: Math.floor(Math.random() * screen.height / 3).toString(),
+            marginLeft: Math.floor(Math.random() * screen.width / 3).toString()});
+     }
+     
     toShow = Math.random() < .129032 
         ? fnord()
         : count.toString();
-    if (count > 100) {
-        window.location.replace("../fnordfnord.html");
-    }
     
     $(".eros")
         .html('<p>' + toShow + '</p>')
         .css('color', Math.floor(Math.random() * 16777215).toString(16))
-        .css("margin-top", Math.floor(Math.random() * screen.height / 2).toString())
         .css("margin-left", Math.floor(Math.random() * screen.width / 2).toString())
+        .css("margin-top", Math.floor(Math.random() * (screen.height - $(".chaos").offset().y)  / 2).toString())
         .show()
         .delay(1500)
         .fadeOut();
